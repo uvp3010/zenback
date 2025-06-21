5defc62aecb6d3b9cb251b04f1fbfec712e5c8fc
uvp3010 <richardalpert3010@gmail.com>
2025-06-21 21:19:24 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
     SESSION_TYPE = 'filesystem'
-    SESSION_COOKIE_SAMESITE = 'Lax'
-    SESSION_COOKIE_SECURE = False  # Change to True in production when using HTTPS
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///users.db')
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True  # Change to True in production when using HTTPS
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AUDIO_FOLDER = "audio"
     # Twilio configuration
